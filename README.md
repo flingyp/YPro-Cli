c15fdcccdbe86f000a344a6ea45731dad9fb83e4
flingyp <1213563369@qq.com>
2022-05-04 19:28:21 +0800

    feat: update README.md

---
+++ README.md
@@ -0,0 +1,19 @@
+<div align="center">
+    <h1>YPro-Cli</h1>
+</div>
+
+## 介绍
+
+用于快速搭建前端开发基础模板和相关基本环境脚手架。
+
+## 基本使用
+
+快速搭建项目基础模板
+
+- `ypro create <app-name>`
+
+- 根据 [awesome-vite](https://github.com/vitejs/awesome-vite#templates) 提供的相关模板，开发者可以自由选择适合自己的项目模板，脚手架直接将项目 Git Clone 到本地。
+
+快速创建 vue 基本页面
+
+- `ypro add <page-name>`
--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ const { program } = require("commander");
 
 // 创建一个基础项目
 program
-  .command("create <app-name> [options]")
+  .command("create <app-name>")
   .description("创建应用的基础模板")
   .action((appName, destination) => {
     // console.log("app-name：", appName);
@@ -14,7 +14,7 @@ program
 
 // 创建页面模块的基本文件结构
 program
-  .command("add <page-name> [options]")
+  .command("add <page-name>")
   .description("创建页面模块的基础文件结构")
   .action((pageName, destination) => {
     // console.log("page-name：", pageName);
